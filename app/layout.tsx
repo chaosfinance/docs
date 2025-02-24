353c11c6ef992e1246427f89a5ddaee4861fe07f
Du, Chengbin <robertduu@gmail.com>
2025-02-24 16:34:47 +0800

    support math

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './global.css';
+import 'katex/dist/katex.css';
+
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
